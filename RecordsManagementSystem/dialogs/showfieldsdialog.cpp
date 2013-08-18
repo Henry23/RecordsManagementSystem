56c98d05b277b5ace3aa6f18886f9e6460b5ab85
Henry23 <gabriel_23hgap@unitec.edu>
2013-08-18 00:01:26 -0600

    Modifica Campos

--- RecordsManagementSystem/dialogs/showfieldsdialog.cpp
+++ RecordsManagementSystem/dialogs/showfieldsdialog.cpp
@@ -6,6 +6,8 @@
 #include <QTableWidgetItem>
 #include <QMessageBox>
 
+#include <qDebug>
+
 ShowFieldsDialog::ShowFieldsDialog(QString fileName, bool modifyMode, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ShowFieldsDialog)
@@ -306,5 +308,121 @@ void ShowFieldsDialog::on_tableWidgetFields_cellChanged(int row, int column)
         //Utilizalos para saber que campo y que propiedad vas a modificar en el archivo
         //Utiliza la siguiente instruccion para obtener el nombre del elemento modificado:
         //ui->tableWidgetFields->item(row, column)->text();
+
+        //we got the list of slipt it by '|'
+        QStringList fieldsProperties = this->getFieldsProperties();
+
+        for ( int i = 0; i < fieldsProperties.size(); i++ )
+        {
+            qDebug() << fieldsProperties.at(i).length();
+        }
+
+
+        //list slipt it by ','
+        QStringList fieldProperties;
+        //list of all the length of all the fields
+       // QStringList index;
+        //for each field that has been slipt by parts
+        QStringList field;
+        for ( int a = 0; a < fieldsProperties.size(); a++ )
+        {
+            fieldProperties = fieldsProperties.at(a).split(",");
+
+            for ( int b = 0; b <fieldProperties.size(); b++ )
+            {
+               /* if ( b == 0 )
+                {
+                    index << fieldProperties.at(b);
+                }
+            */
+                if ( a == row)
+                {
+                    field << fieldProperties.at(b);
+                }
+             }
+         }
+
+         RecordsFile file(this->fileName.toStdString());
+
+         unsigned int length1 = 0;
+
+         unsigned int length2 = 0;
+
+         int tPositionRow = 0;
+
+         int tPositionColumn = 0;
+
+         //fixed
+         for ( int i = 0; i < row; i++ )
+         {
+              tPositionRow +=  fieldsProperties.at(i).length() + 1;
+
+            /* if ( index.at(i).toInt() > 9)
+             {
+                 tPositionRow +=  index.at(i).toInt() + 2;
+             } else if ( index.at(i).toInt() < 9)
+             {
+                   tPositionRow +=  index.at(i).toInt() + 1;
+             }*/
+         }
+
+            //fixed
+         for ( int i = 0; i < column; i++ )
+         {
+             tPositionColumn +=  field.at(i).length()+1;
+          }
+
+
+         length1 = ( this->getLengthOfTheNumberOfFields() + 1 ) + (tPositionRow) + tPositionColumn;
+
+         length2 = file.fileLength() - length1-1;
+
+         qDebug() << " length1 " <<length1;
+
+         qDebug() << " length2 " <<length2;
+
+         char *buffer1 = new char[length1];
+
+         char *buffer2 = new char[length2];
+
+         file.read( buffer1 ,length1 );
+
+         file.seek( length1 + field.at(column).length() );
+
+         file.read( buffer2 , length2 );
+
+         file.close();
+
+         qDebug() << buffer1;
+
+         qDebug() << buffer2;
+
+         const char *path = this->fileName.toStdString().c_str();
+
+         remove( path );
+
+
+         RecordsFile create;
+
+         //Check if there is a problem while creating the file
+
+         if ( !create.open(this->fileName.toStdString(),  ios::out) )
+         {
+             QMessageBox::critical(this, tr("Error"), tr("An error occurred while trying to create the file"));
+         }
+
+         const char * changeField = ui->tableWidgetFields->item(row, column)->text().toStdString().c_str();
+
+         create.write( buffer1, length1);
+
+         create.write( changeField,  ui->tableWidgetFields->item(row, column)->text().length());
+
+         create.write( buffer2 ,length2);
+
+         create.close();
+
+         delete [] buffer1;
+         delete [] buffer2;
+
     }
 }
